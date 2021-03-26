764d8026fa4d16ce3793589a71876469f65eb64e
Alex Dunae <alex@dunae.ca>
2021-03-26 13:33:22 -0700

    Allow setting starting point dynamically

--- src/anywhere-solar-system.cr
+++ src/anywhere-solar-system.cr
@@ -2,14 +2,18 @@ require "http/server"
 require "option_parser"
 require "html"
 
-INITIAL_COORDINATES = {-123.103767, 49.273251}
+INITIAL_COORDINATES = {49.273251, -123.103767}
+LATITUDE_RANGE      = (-90..90)
+LONGITUDE_RANGE     = (-180..180)
+
+SUN_SIZE_RANGE = (0.01..500.0)
 
 # all sizes are in meters
-SUN_SIZE = 1_392_700_000
+ACTUAL_SUN_SIZE = 1_392_700_000
 
 # tuples of (Name, OrbitRadius, CelestialBodyRadius)
 PLANETS = [
-  {"Sun", 0, SUN_SIZE},
+  {"Sun", 0, ACTUAL_SUN_SIZE},
   {"Mercury", 57_900_000_000, 4_878_000},
   {"Venus", 108_160_000_000, 12_104_000},
   {"Earth", 149_600_000_000, 12_756_000},
@@ -45,6 +49,8 @@ def radians(degrees)
   degrees * Math::PI / 180
 end
 
+EARTH_RADIUS = 6378137.0
+
 # https://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
 def offset(lat_long : LatLng, x_y_in_meters : LatLng) : LatLng
   lat, long = lat_long
@@ -55,8 +61,10 @@ def offset(lat_long : LatLng, x_y_in_meters : LatLng) : LatLng
   }
 end
 
-def build_placemark(name, orbit_radius, body_radius)
-  coordinates = circle_coordinates(orbit_radius).map { |x_y| offset(INITIAL_COORDINATES, x_y) }
+def build_placemark(centre_lat_long : LatLng, name : String, orbit_radius : Float64, body_radius : Float64)
+  puts "Build placemark: #{centre_lat_long} -> #{name} orbit radius is #{orbit_radius}"
+  inverted = {centre_lat_long[1], centre_lat_long[0]}
+  coordinates = circle_coordinates(orbit_radius).map { |x_y| offset(inverted, x_y) }
   placemark = Placemark.new(name: name, coordinates: coordinates, body_radius: body_radius)
 end
 
@@ -79,15 +87,15 @@ def kml_circle(placemark : Placemark)
   XML
 end
 
-def generate_kml(sun_size : Float64) : String
-  ratio = sun_size / SUN_SIZE
+def generate_kml(sun_size : Float64, centre_lat_long : LatLng) : String
+  ratio = sun_size / ACTUAL_SUN_SIZE
   puts "Calculating for sun size of #{sun_size}m... - ratio: #{ratio}"
 
   kml_circles = PLANETS.map do |planet|
     name, actual_orbit_radius, actual_body_radius = planet[0], planet[1], planet[2]
     scaled_orbit_radius = actual_orbit_radius * ratio
     scaled_body_radius = actual_body_radius * ratio
-    placemark = build_placemark(name, scaled_orbit_radius, scaled_body_radius)
+    placemark = build_placemark(centre_lat_long, name, scaled_orbit_radius, scaled_body_radius)
     kml_circle(placemark)
   end
 
@@ -117,18 +125,24 @@ GMAP_API_KEY = ENV["GMAP_API_KEY"]
 
 server = HTTP::Server.new do |context|
   params = context.request.query_params
-  raw_input = params.fetch("sun_size", "5")
-  sun_size = raw_input.chomp.to_f / 1.0
-  raise "Sun value out of range" if sun_size <= 0 || sun_size > 100_000
+  sun_size = params.fetch("sun_size", "5").to_f
+  raise "Sun value out of range" unless SUN_SIZE_RANGE.includes?(sun_size)
 
-  kml_endpoint = "http://#{context.request.host_with_port}/kml?sun_size=#{sun_size}"
+  latitude = params.fetch("latitude", INITIAL_COORDINATES[0].to_s).to_f
+  raise "Latitude out of range" unless LATITUDE_RANGE.includes?(latitude)
+
+  longitude = params.fetch("longitude", INITIAL_COORDINATES[1].to_s).to_f
+  raise "Longitude out of range" unless LONGITUDE_RANGE.includes?(longitude)
+
+  kml_endpoint = "http://#{context.request.host_with_port}/kml?sun_size=#{sun_size}&latitude=#{latitude}&longitude=#{longitude}&s=#{Time.utc.to_unix}"
+  puts "Endpoint: #{kml_endpoint}"
 
   if context.request.path == "/kml"
     context.response.content_type = "text/xml"
-    context.response.print generate_kml(sun_size)
+    context.response.print generate_kml(sun_size, {latitude, longitude})
   else
     context.response.content_type = "text/html"
-    context.response.print generate_html(sun_size, kml_endpoint)
+    context.response.print generate_html(sun_size, {latitude, longitude}, kml_endpoint)
   end
 end
 
@@ -137,7 +151,7 @@ puts "Listening on http://#{address}"
 
 server.listen
 
-def generate_html(sun_size, kml_endpoint)
+def generate_html(sun_size, initial_coordinates : LatLng, kml_endpoint)
   html = <<-HTML
   <!DOCTYPE html>
     <html>
@@ -150,20 +164,20 @@ def generate_html(sun_size, kml_endpoint)
         </style>
         <script>
           var map;
+          var kmlLayer;
           var src = '#{HTML.escape(kml_endpoint)}';
 
           function initMap() {
             map = new google.maps.Map(document.getElementById('map'), {
-              center: new google.maps.LatLng(#{INITIAL_COORDINATES[1]}, #{INITIAL_COORDINATES[0]}),
+              center: new google.maps.LatLng(#{initial_coordinates[0]}, #{initial_coordinates[1]}),
               zoom: 2,
               mapTypeId: 'terrain'
             });
 
+            map.setZoom(7);
 
-            var kmlLayer = new google.maps.KmlLayer(src, {
-              preserveViewport: false,
-              map: map
-            });
+
+            kmlLayer = new google.maps.KmlLayer(src, { preserveViewport: false, map: map });
           }
         </script>
       </head>
@@ -173,10 +187,22 @@ def generate_html(sun_size, kml_endpoint)
         <p>Just like the <a href="http://www.swedensolarsystem.se/en/">Sweden Solar System</a></p>
 
         <form method="get" action="/">
-          <label for="sun_size">Sun size (in meters)</label>
-          <input type="number" name="sun_size" id="sun_size" value="#{sun_size}" min="0" max="100000">
+          <label>Sun size (in meters)
+            <input type="number" name="sun_size" id="sun_size" value="#{sun_size}" min="#{SUN_SIZE_RANGE.begin}" max="#{SUN_SIZE_RANGE.end}" step="0.01">
+          <br>
+          <label>Sun latitude
+            <input type="text" name="latitude" id="latitude" value="#{initial_coordinates[0]}">
+          </label>
+          <br>
+          <label>Sun longitude
+            <input type="text" name="longitude" id="longitude" value="#{initial_coordinates[1]}">
+          </label>
+          <br>
           <input type="submit">
         </form>
+
+        <br>
+        <button id="geolocate">Use my current location</button>
 
         <div id="map"></div>
 
@@ -184,6 +210,26 @@ def generate_html(sun_size, kml_endpoint)
         <p>Source code at <a href="https://github.com/alexdunae/anywhere-solar-system">github.com/alexdunae/anywhere-solar-system</a></p>
 
         <script async defer src="https://maps.googleapis.com/maps/api/js?key=#{GMAP_API_KEY}&callback=initMap"></script>
+        <script>
+        function geolocate(event) {
+            event.preventDefault();
+            function success(position) {
+              document.getElementById('latitude').value = position.coords.latitude;
+              document.getElementById('longitude').value = position.coords.longitude;
+            }
+
+            if(!navigator.geolocation) {
+              alert('Geolocation is not supported by your browser');
+            } else {
+              status.textContent = 'Locating…';
+              navigator.geolocation.getCurrentPosition(success, function () {
+                  alert('Unable to retrieve your location');
+              });
+            }
+          }
+
+          document.querySelector('#geolocate').addEventListener('click', geolocate);
+        </script>
       </body>
     </html>
   HTML
